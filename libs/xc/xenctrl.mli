59fb2235d71ea1fdbbac7a7e877eeeaeb4e63e62
Andrew Cooper <andrew.cooper3@citrix.com>
2023-01-03 10:20:46 +0000

    Drop trailing whitespace
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- libs/xc/xenctrl.mli
+++ libs/xc/xenctrl.mli
@@ -136,8 +136,8 @@ type shutdown_reason = Poweroff | Reboot | Suspend | Crash | Watchdog | Soft_res
 
 exception Error of string
 type handle
-val interface_open : unit -> handle 
-val interface_close : handle -> unit 
+val interface_open : unit -> handle
+val interface_close : handle -> unit
 
 (** [with_intf f] runs [f] with a global handle that is opened on demand
  * and kept open. Conceptually, a client should use either
@@ -153,101 +153,100 @@ val get_handle: unit -> handle option
 val close_handle: unit -> unit
 
 val domain_create : handle -> domctl_create_config -> domid
-  
-val domain_sethandle : handle -> domid -> string -> unit 
+
+val domain_sethandle : handle -> domid -> string -> unit
 val domain_max_vcpus : handle -> domid -> int -> unit
-  
-val domain_pause : handle -> domid -> unit 
-val domain_unpause : handle -> domid -> unit 
+
+val domain_pause : handle -> domid -> unit
+val domain_unpause : handle -> domid -> unit
 val domain_resume_fast : handle -> domid -> unit
-  
-val domain_destroy : handle -> domid -> unit 
+
+val domain_destroy : handle -> domid -> unit
 val domain_shutdown : handle -> domid -> shutdown_reason -> unit
-  
+
 val _domain_getinfolist : handle -> domid -> int -> domaininfo list
-  
+
 val domain_getinfolist : handle -> domid -> domaininfo list
 val domain_getinfo : handle -> domid -> domaininfo
-  
+
 val domain_get_vcpuinfo : handle -> int -> int -> vcpuinfo
-  
+
 val domain_get_runstate_info : handle -> int -> runstateinfo
-  
+
 val domain_ioport_permission: handle -> domid -> int -> int -> bool -> unit
-       
+
 val domain_iomem_permission: handle -> domid -> nativeint -> nativeint -> bool -> unit
-       
+
 val domain_irq_permission: handle -> domid -> int -> bool -> unit
-       
+
 val vcpu_affinity_set : handle -> domid -> int -> bool array -> unit
-  
+
 val vcpu_affinity_get : handle -> domid -> int -> bool array
-  
+
 val vcpu_context_get : handle -> domid -> int -> string
-  
-val sched_id : handle -> int 
+
+val sched_id : handle -> int
 val sched_credit_domain_set : handle -> domid -> sched_control -> unit
-  
+
 val sched_credit_domain_get : handle -> domid -> sched_control
-  
+
 val shadow_allocation_set : handle -> domid -> int -> unit
-  
+
 val shadow_allocation_get : handle -> domid -> int
-  
+
 val evtchn_alloc_unbound : handle -> domid -> domid -> int
-  
-val evtchn_reset : handle -> domid -> unit 
-val readconsolering : handle -> string 
-val send_debug_keys : handle -> string -> unit 
-val physinfo : handle -> physinfo 
-val pcpu_info: handle -> int -> int64 array 
+
+val evtchn_reset : handle -> domid -> unit
+val readconsolering : handle -> string
+val send_debug_keys : handle -> string -> unit
+val physinfo : handle -> physinfo
+val pcpu_info: handle -> int -> int64 array
 val domain_setmaxmem : handle -> domid -> int64 -> unit
-  
+
 val domain_set_memmap_limit : handle -> domid -> int64 -> unit
-  
+
 val domain_memory_increase_reservation :
   handle -> domid -> int64 -> unit
-  
+
 val map_foreign_range :
   handle -> domid -> int -> nativeint -> Xenmmap.mmap_interface
-  
+
 
 val domain_assign_device: handle -> domid -> (int * int * int * int) -> unit
-       
+
 val domain_deassign_device: handle -> domid -> (int * int * int * int) -> unit
-       
+
 val domain_test_assign_device: handle -> domid -> (int * int * int * int) -> bool
-       
+
 
 val hvm_check_pvdriver : handle -> domid -> bool
-  
-val version : handle -> version 
+
+val version : handle -> version
 val version_compile_info : handle -> compile_info
-  
-val version_changeset : handle -> string 
+
+val version_changeset : handle -> string
 val version_capabilities : handle -> string
-  
+
 
 type featureset_index = Featureset_raw | Featureset_host | Featureset_pv | Featureset_hvm | Featureset_pv_max | Featureset_hvm_max
-val get_cpu_featureset : handle -> featureset_index -> int64 array 
-val get_featureset : handle -> featureset_index -> int64 array 
+val get_cpu_featureset : handle -> featureset_index -> int64 array
+val get_featureset : handle -> featureset_index -> int64 array
 
-val upgrade_oldstyle_featuremask: handle -> int64 array -> bool -> int64 array 
-val oldstyle_featuremask: handle -> int64 array 
+val upgrade_oldstyle_featuremask: handle -> int64 array -> bool -> int64 array
+val oldstyle_featuremask: handle -> int64 array
 
-val pages_to_kib : int64 -> int64 
+val pages_to_kib : int64 -> int64
 val pages_to_mib : int64 -> int64
 val watchdog : handle -> int -> int32 -> int
-  
+
 
 val domain_set_machine_address_size: handle -> domid -> int -> unit
-  
+
 val domain_get_machine_address_size: handle -> domid -> int
-       
+
 
 val domain_cpuid_set: handle -> domid -> (int64 * (int64 option))
                         -> string option array
                         -> string option array
-       
+
 val domain_cpuid_apply_policy: handle -> domid -> unit
-       
