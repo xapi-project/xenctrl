af59317704d0cdfeedc360dba1c632544df1c089
Gang Ji <gang.ji@cloud.com>
2025-08-04 17:11:01 +0800

    CP-308857: rebase on top of xen.spec/PR#481
    Revert "CP-308452: Update xenctrl library the same way patched xen does"
    
    This reverts commit 52f32582e43776e36680b596e6514aec3124dee1.
    
    Revert the change as xen will be patched in a new way:
    domain_get_runstate_info and runstateinfo will not be changed, instead a
    new API: domain_get_runstate_info2 is added with new type runstateinfo2.

--- libs/xc/xenctrl.ml
+++ libs/xc/xenctrl.ml
@@ -99,7 +99,6 @@ type runstateinfo = {
   time3 : int64;
   time4 : int64;
   time5 : int64;
-  runnable : int64;
 }
 
 type domaininfo =
--- libs/xc/xenctrl.mli
+++ libs/xc/xenctrl.mli
@@ -91,7 +91,6 @@ type runstateinfo = {
   time3 : int64;
   time4 : int64;
   time5 : int64;
-  runnable : int64;
 }
 
 type domaininfo = {
